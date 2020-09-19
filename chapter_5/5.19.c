13c695760053daaee45008c65a1f572664950ab7
dicce3 <53824061+dicce3@users.noreply.github.com>
2020-09-19 19:43:24 +0800

    Update 5.19.c
    第一个FOR循环有问题

--- chapter_5/5.19.c
+++ chapter_5/5.19.c
@@ -8,7 +8,7 @@ void psum_4x1a(float a[], float p[], long n) {
   float tmp0, tmp1, tmp2, tmp3;
   last_val = p[0] = a[0];
 
-  for (i = 1; i < n - 4; i++) {
+  for (i = 1; i < n - 4; i+=4) { //原来是i++已更正。
     tmp0 = last_val + a[i];
     tmp1 = tmp0 + a[i+1];
     tmp2 = tmp1 + a[i+2];
